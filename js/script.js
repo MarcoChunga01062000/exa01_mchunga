669d08eab1a617d5f93546534a669fe370dd3664
MarcoChunga01062000 <marcochunga@upeu.edu.pe>
2018-04-26 18:11:06 -0500

    V 3.0

--- js/script.js
+++ js/script.js
@@ -1,65 +1,49 @@
-function lap(){
-  var precio1 = document.getElementById("productos").value;
-  document.getElementById("precio").innerHTML=precio1;
+var tb = document.getElementById("info");
+var fil=0,col=0;
+var op = false;
+function agregar(){
+	var prod = document.getElementById("productos").value;
+	var precio = document.getElementById("precio").value;
+	var cantidad = document.getElementById("cantidad").value;
+	if(verificar(prod)){
+		var x = tb.rows[fil].cells;
+		var x2 =parseInt(x[2].innerHTML)+parseInt(cantidad)
+		x[2].innerHTML=x2;
+		x[3].innerHTML=parseInt(precio)*x2;
+		resultado();
+	 	op= false;
+		alert("Agregado correctamente el segundo objeto...!")
+	 }else{
+	 	var n = tb.rows.length;
+		var row = tb.insertRow(n);
+		var cell1 = row.insertCell(0);
+		var cell2 = row.insertCell(1);
+		var cell3 = row.insertCell(2);
+		var cell4 = row.insertCell(3);
+		cell1.innerHTML=prod;
+		cell2.innerHTML=precio;
+		cell3.innerHTML=cantidad;
+		cell4.innerHTML=precio*cantidad;
+		resultado();
+	}
 }
-function AgregarProd(){
-  var tb = document.getElementById("info");
-  var productoss = document.getElementById("productos").value;
-  var precio = document.getElementById("precio").innerHTML;
-  var cantidad = document.getElementById("cantidad").value;
-  var Total = (precio * cantidad);
-  var fila="<tr><td id='nombre'>"+productoss+"</td><td>"+precio+"</td><td>"+cantidad+"</td><td>"+Total+"</td><td><a href='#' onclick='eliminar(this)'><img src='imagenes/d.png'></td></tr>";
-  var fil = document.createElement("tr");
-  fil.innerHTML=fila;
-  tb.appendChild(fil);
-  CalcSubTotal();
-  CalcIGV();
-  TOTAL();
-  limpiar();
-  mensaje("¡Registro guardado correctamente...!");
+function verificar(p){
+	for(var i=0; i<tb.rows.length; i++)
+	{
+	if(tb.rows[i].cells[0].innerHTML==p){
+		op = true;
+		fil=i;col=0;
+	}
 }
-function CalcSubTotal(){
-  var SubTotal = 0;
-  var info = document.getElementById("info");
-  for (var i = 1; i < info.rows.length; i++) {
-      var st = parseFloat(info.rows[i].cells[3].innerText);
-      SubTotal = SubTotal+st;
-  }
-    document.getElementById("SubTotal").innerText=SubTotal.toFixed(2);
+return op;
 }
-function CalcIGV(){
-  var cigv = 0;
-  var info = document.getElementById("info");
-  for (var i = 1; i < info.rows.length; i++) {
-      var is = parseFloat(info.rows[i].cells[3].innerText);
-      cigv=cigv+is;
-  }
-  tigv = cigv * 0.18;
-    document.getElementById("IGV").innerText=tigv.toFixed(2);
-}
-function TOTAL(){
-  var x = parseFloat(document.getElementById("SubTotal").innerHTML);
-  var y = parseFloat(document.getElementById("IGV").innerHTML);
-  var TOTAL = x+y;
-  document.getElementById("Total").innerText=TOTAL.toFixed(2);
-}
-function mensaje(x){
-  alert(x);
-}
-
-function eliminar(p){
-    var opcion = confirm("¿Desea Eliminar...?");
-    if (opcion == true) {
-        var i = p.parentNode.parentNode.rowIndex;
-        document.getElementById("info").deleteRow(i);
-        mensaje("Registro Eliminado correctamente...!");
-    } else {
-        mensaje("Registro no se eliminó...!");
-    }
-}
-function limpiar(){
-    document.getElementById("productos").value = "";
-    document.getElementById("precio").innerHTML = "";
-    document.getElementById("cantidad").value = "";
-    document.getElementById("nombre").focus();
+function resultado(){
+var sum = 0;
+ for(var i=1; i<tb.rows.length; i++)
+	{
+		sum = sum + parseInt(tb.rows[i].cells[3].innerHTML);
+	}
+	document.getElementById("SubTotal").value=sum;
+	document.getElementById("IGV").value=(sum*0.18).toFixed(2);
+	document.getElementById("Total").value=sum+sum*0.18;
 }
